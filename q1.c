834073541e79314a571d9debd6ecf5b34f8f5c14
rajumondal-tech <rajumondal.dev.@gmail.com>
2025-12-12 03:01:34 +0530

    format document

--- q1.c
+++ q1.c
@@ -4,15 +4,17 @@ int main()
     float value, result;
     int choice;
     printf("enter 1 for Celsius to Fahrenheit.\nenter 2 for Fehrenheit to Celsius \n");
-     scanf("%d", &choice);
-   printf("enter value ");
+    scanf("%d", &choice);
+    printf("enter value ");
     scanf("%f", &value);
-    if (choice == 1){
+    if (choice == 1)
+    {
         result = (value * 9.0 / 5.0) + 32;
     }
-        else
-        {result = (value - 32) * 5.0 / 9.0;
-        }
-printf("Converted result= %f", result);
-return 0;
+    else
+    {
+        result = (value - 32) * 5.0 / 9.0;
+    }
+    printf("Converted result= %f", result);
+    return 0;
 }
