b33ec23737928a37801b2a9f07711383600c45a4
rajumondal-tech <rajumondal.dev.@gmail.com>
2025-12-12 04:29:49 +0530

    formated

--- q2.c
+++ q2.c
@@ -1,13 +1,28 @@
-//Sort 3 input numbers (non-decreasing order).
+// Sort 3 input numbers (non-decreasing order).
 #include <stdio.h>
 int main()
 {
     int a, b, c;
     printf("enter 3 number\n");
     scanf("%d %d %d", &a, &b, &c);
-    if (a > b) {int x = a; a = b; b = x; }
-    if (a > c) {int x = a; a = c; c = x; }
-    if (b > c) {int x = b; b = c; c = x;}
-printf("%d %d %d\n", a, b, c);
-return 0;
+    if (a > b)
+    {
+        int x = a;
+        a = b;
+        b = x;
+    }
+    if (a > c)
+    {
+        int x = a;
+        a = c;
+        c = x;
+    }
+    if (b > c)
+    {
+        int x = b;
+        b = c;
+        c = x;
+    }
+    printf("%d %d %d\n", a, b, c);
+    return 0;
 }
