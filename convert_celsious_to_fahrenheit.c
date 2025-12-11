829e20e7cc6d46f9f7fc9829a0843b33155826fe
rajumondal-tech <rajumondal.dev.@gmail.com>
2025-12-12 00:58:09 +0530

    Celsius to Fahrenheit Calculator

---
+++ convert_celsious_to_fahrenheit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    float value, result;
+    int choice;
+    printf("enter 1 for Celsius to Fahrenheit.\nenter 2 for Fehrenheit to Celsius \n");
+     scanf("%d", &choice);
+   printf("enter value ");
+    scanf("%f", &value);
+    if (choice == 1){
+        result = (value * 9.0 / 5.0) + 32;
+    }
+        else
+        {result = (value - 32) * 5.0 / 9.0;
+        }
+printf("Converted result= %f", result);
+return 0;
+}
